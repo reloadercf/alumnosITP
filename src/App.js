fcf79c4d2d9cac39790ec4b10b39135c0c5a5668
reloadercf <elcfm1@gmail.com>
2020-02-20 11:02:28 -0600

    Consumo a la api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+
+
 
 function App() {
+
+  let [gifMeme,obtenerGIF]=useState({})
+  let llave="iWKgEQDoiJeTYfR71hHj3vXLPEPatbDJ&limi"
+  let consultarAPI = async()=>{
+    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${llave}`
+    // console.log(resultado.data[0])
+    let respuesta = await fetch(url)
+    let memes = await respuesta.json()
+    console.log(memes)
+    //agregar el resultado de la API al state (similar a this.setState)
+    obtenerGIF(memes.data[0])
+
+  }
+
+  useEffect(
+    ()=>{
+      consultarAPI()
+    },[]
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
